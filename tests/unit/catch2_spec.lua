9b9599fd02e41acfa5c88e07c63c4e28efd5f7f0
Brian Silver <github@briansilver.net>
2024-11-25 14:14:57 -0500

    test: add unit test for CATCH prefix used in catch2

--- tests/unit/catch2_spec.lua
+++ tests/unit/catch2_spec.lua
@@ -49,4 +49,51 @@ describe("catch2.parse_positions", function()
     assert.are.same(expected_positions[2][2][1], actual_positions[2][2][1])
     assert.are.same(expected_positions[3][1], actual_positions[3][1])
   end)
+
+  it("discovers CATCH_TEST_CASE", function()
+    local test_file = (vim.uv or vim.loop).cwd()
+      .. "/tests/unit/data/framework/catch2/CATCH_TEST_CASE_test.cpp"
+    local actual_positions = catch2.parse_positions(test_file):to_list()
+    local expected_positions = {
+      {
+        id = test_file,
+        name = "CATCH_TEST_CASE_test.cpp",
+        path = test_file,
+        range = { 0, 0, 14, 0 },
+        type = "file",
+      },
+      {
+        {
+          id = ("%s::%s"):format(test_file, "my_tests"),
+          name = "my_tests",
+          path = test_file,
+          range = { 4, 0, 8, 1 },
+          type = "namespace",
+        },
+        {
+          {
+            id = ("%s::%s::%s"):format(test_file, "my_tests", "First"),
+            name = "First",
+            path = test_file,
+            range = { 6, 0, 6, 60 },
+            type = "test",
+          },
+        },
+      },
+      {
+        {
+          id = ("%s::%s"):format(test_file, "Second"),
+          name = "Second",
+          path = test_file,
+          range = { 10, 0, 13, 1 },
+          type = "test",
+        },
+      },
+    }
+
+    assert.are.same(expected_positions[1], actual_positions[1])
+    assert.are.same(expected_positions[2][1], actual_positions[2][1])
+    assert.are.same(expected_positions[2][2][1], actual_positions[2][2][1])
+    assert.are.same(expected_positions[3][1], actual_positions[3][1])
+  end)
 end)
